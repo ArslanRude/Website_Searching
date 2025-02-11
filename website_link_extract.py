399347077f2d2b539aa1af923c970c6784e2ff5f
Arslan <arslanmorza644@gmail.com>
2025-02-11 22:05:13 +0500

    a

--- website_link_extract.py
+++ website_link_extract.py
@@ -7,28 +7,23 @@ def get_link_from_website(link):
         soup = BeautifulSoup(geturl.text, "html.parser")
         footer = soup.find("footer")
         links = [a['href'] for a in footer.find_all('a', href=True)]
-        links_from_website = {'linkedin_url':{'url' : '','found': False},
-                        'facebook_url':{'url' : '','found': False},
-                        'twitter_url':{'url' : '','found': False},
-                        'youtube_url':{'url' : '','found': False},
-                        'email' : {'url' : '','found': False},
+        links_from_website = {'linkedin_url':'',
+                        'facebook_url':'',
+                        'twitter_url':'',
+                        'youtube_url':'',
+                        'email' : '',
                         }
         for i in links:
             if i.count('linkedin.'):
-                links_from_website['linkedin_url']['url'] = i
-                links_from_website['linkedin_url']['found'] = True
+                links_from_website['linkedin_url'] = i
             if i.count('facebook.'):
-                links_from_website['facebook_url']['url'] = i
-                links_from_website['facebook_url']['found'] = True
+                links_from_website['facebook_url'] = i
             if (i.count('twitter.') or i.count('x.')):
-                links_from_website['twitter_url']['url'] = i
-                links_from_website['twitter_url']['found'] = True
+                links_from_website['twitter_url'] = i
             if i.count('youtube.'):
-                links_from_website['youtube_url']['url'] = i
-                links_from_website['youtube_url']['found'] = True
+                links_from_website['youtube_url'] = i
             if i.count('@'):
-                links_from_website['email']['url'] = i
-                links_from_website['email']['found'] = True
+                links_from_website['email'] = i
         return links_from_website
     except:
         return ""
